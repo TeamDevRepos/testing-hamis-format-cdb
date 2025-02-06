779fee3e6429e2f1a8dff93a53401c6564a9fab7
DiegoCG171 <diego.cerongutierrez@gmail.com>
2025-02-06 03:19:31 -0600

    fix: test skill card effect - 900002003

--- script/c900002003.lua
+++ script/c900002003.lua
@@ -1,16 +1,31 @@
 local s,id=GetID()
-
 function s.initial_effect(c)
 	aux.AddSkillProcedure(c,1,false,s.flipcon,s.flipop)
+	-- Rastrea la cantidad de LP perdidos
+	if not s.global_check then
+		s.global_check=true
+		local ge1=Effect.CreateEffect(c)
+		ge1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+		ge1:SetCode(EVENT_DAMAGE)
+		ge1:SetOperation(s.checkop)
+		Duel.RegisterEffect(ge1,0)
+	end
+end
+
+function s.checkop(e,tp,eg,ep,ev,re,r,rp)
+	if ep~=tp then return end
+	if ev>=1500 then
+		Duel.RegisterFlagEffect(tp,id,RESET_PHASE+PHASE_DRAW,0,1)
+	end
 end
 
 function s.flipcon(e,tp,eg,ep,ev,re,r,rp)
-	-- Verifica si el jugador ha perdido 1500 LP
-	return aux.CanActivateSkill(tp) and Duel.GetFlagEffect(tp,id)<Duel.GetLP(tp)//1500
+	-- Verifica si el jugador ha perdido al menos 1500 LP antes de su Draw Phase
+	return aux.CanActivateSkill(tp) and Duel.GetFlagEffect(tp,id)>0 and Duel.GetTurnCount()>1 and Duel.IsTurnPlayer(tp) and Duel.GetCurrentPhase()==PHASE_DRAW
 end
 
 function s.flipop(e,tp,eg,ep,ev,re,r,rp)
-	Duel.RegisterFlagEffect(tp,id,0,0,0)
+	Duel.ResetFlagEffect(tp,id)
 	-- Reemplaza el robo normal por un monstruo aleatorio de atributo TIERRA
 	local g=Duel.GetMatchingGroup(Card.IsAttribute,tp,LOCATION_DECK,0,nil,ATTRIBUTE_EARTH)
 	if #g>0 then
