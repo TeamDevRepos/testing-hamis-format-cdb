3f004be654ad56a7ffdf985288233c05ebe06ea4
DiegoCG171 <diego.cerongutierrez@gmail.com>
2025-02-08 04:28:01 -0600

    test: change cover - 900002002

--- script/c900002002.lua
+++ script/c900002002.lua
@@ -4,10 +4,6 @@ function s.initial_effect(c)
 	--skill
 	aux.AddPreDrawSkillProcedure(c,1,false,s.flipcon,s.flipop)
 
-	-- Personalizar el cover usando Auxiliary.GetCover
-    local coverNum = 302000002  -- Cambia este número según el diseño deseado
-    local coverID = Auxiliary.GetCover(c, coverNum)
-
 	aux.GlobalCheck(s,function()
 		s[0]=nil
 		s[1]=nil
@@ -41,12 +37,6 @@ end
 function s.flipop(e,tp,eg,ep,ev,re,r,rp)
 	--ask if you want to activate the skill or not
 	if not Duel.SelectYesNo(tp,aux.Stringid(id,0)) then return end
-	
-	-- Aplicar el cover personalizado
-    local coverNum = 302000002  -- Número personalizado para el cover
-    local coverID = Auxiliary.GetCover(e:GetHandler(), coverNum)
-    Duel.Hint(HINT_SKILL_COVER, tp, coverID)
-
 	--draw replace
 	local dt=Duel.GetDrawCount(tp)
 	if dt~=0 then
