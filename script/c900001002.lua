5616200a767064cfba9c6612bbc6e8e54f2c5e4e
DiegoCG171 <diego.cerongutierrez@gmail.com>
2025-02-15 03:44:37 -0600

    test: effect card - 900001002

--- script/c900001002.lua
+++ script/c900001002.lua
@@ -7,7 +7,7 @@ function s.initial_effect(c)
 	-- Set Fire Formation Spells/Traps
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,0))
-	e1:SetCategory(CATEGORY_TOGRAVE+CATEGORY_SEARCH+CATEGORY_TOFIELD)
+	e1:SetCategory(CATEGORY_TOGRAVE+CATEGORY_SEARCH)
 	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
 	e1:SetProperty(EFFECT_FLAG_DELAY)
 	e1:SetCode(EVENT_SPSUMMON_SUCCESS)
